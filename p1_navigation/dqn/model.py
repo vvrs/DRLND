77f089c382fb9288b0d4fdff4b8af1bc80d83f62
vvrs <vishnu.rudrasamudram@gmail.com>
2018-11-18 17:03:38 -0500

    Added navigation project files...

---
+++ p1_navigation/dqn/model.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(QNetwork, self).__init__()
+        fc= [128, 128]
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc[0])
+        self.fc2 = nn.Linear(fc[0], fc[1])
+        self.fc3 = nn.Linear(fc[1], action_size)
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
